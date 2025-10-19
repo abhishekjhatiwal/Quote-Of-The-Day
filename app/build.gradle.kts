89c511e1d9f47f6bac5558d10f3fd68e2fd9f3fb
Abhishekverma2006 <j472812@gmail.com>
2025-10-19 22:09:42 +0530

    Forth commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // Material 3 components
+    implementation("androidx.compose.material:material-icons-extended:1.7.8") // Use the latest version
 }
--- app/src/main/java/com/example/quoteoftheday/screen/HomeScreen.kt
+++ app/src/main/java/com/example/quoteoftheday/screen/HomeScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.FormatQuote
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Card

d13f46fdfba14dfec0319696e6f6d87c4225ec29
Abhishekverma2006 <j472812@gmail.com>
2025-10-19 23:44:04 +0530

    19-10-2025 23.44

--- app/src/main/java/com/example/quoteoftheday/screen/ActionButton.kt
+++ app/src/main/java/com/example/quoteoftheday/screen/ActionButton.kt
@@ -16,11 +16,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun ActionButton(
-    icon: androidx.compose.ui.graphics.vector.ImageVector,
+    icon: ImageVector,
     text: String,
     color: Color,
     onClick: () -> Unit
